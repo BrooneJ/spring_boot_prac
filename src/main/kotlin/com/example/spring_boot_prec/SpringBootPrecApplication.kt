dea6b842ad5ddf0ea941aabbaef40b7781d6100e
HyungwonJin <hwjin92@gmail.com>
2025-01-02 11:22:21 +0900

    first commit

---
+++ src/main/kotlin/com/example/spring_boot_prec/SpringBootPrecApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_boot_prec
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPrecApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringBootPrecApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/spring_boot_prec/SpringBootPrecApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring_boot_prec
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootPrecApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
