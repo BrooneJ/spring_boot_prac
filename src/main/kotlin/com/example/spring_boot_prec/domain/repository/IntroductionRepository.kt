712fd99200dd94255e3e0ffdf1cd03aedcc22d04
HyungwonJin <hwjin92@gmail.com>
2025-01-03 23:21:08 +0900

    feat: implements repository

--- src/main/kotlin/com/example/spring_boot_prec/domain/repository/IntroductionRepository.kt
+++ src/main/kotlin/com/example/spring_boot_prec/domain/repository/IntroductionRepository.kt
@@ -3,4 +3,8 @@ package com.example.spring_boot_prec.domain.repository
 import com.example.spring_boot_prec.domain.entity.Introduction
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface IntroductionRepository : JpaRepository<Introduction, Long>
+interface IntroductionRepository : JpaRepository<Introduction, Long> {
+
+  fun findAllByIsActive(isActive: Boolean): List<Introduction>
+  
+}
