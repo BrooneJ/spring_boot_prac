c8cc9cea00d598e1415e5d4ac9fe9487d20da9be
HyungwonJin <hwjin92@gmail.com>
2025-01-04 21:13:40 +0900

    feat: implements interceptor

--- src/main/kotlin/com/example/spring_boot_prec/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/example/spring_boot_prec/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,24 @@
 package com.example.spring_boot_prec.presentation.interceptor
 
+import com.example.spring_boot_prec.domain.entity.HttpInterface
+import com.example.spring_boot_prec.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+  private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+  override fun afterCompletion(
+    request: HttpServletRequest,
+    response: HttpServletResponse,
+    handler: Any,
+    ex: Exception?
+  ) {
+    val httpInterface = HttpInterface(request)
+    httpInterfaceRepository.save(httpInterface)
+  }
 }
--- src/main/kotlin/com/example/spring_boot_prec/presentation/interceptor/PresentationInterceptorConfig.kt
+++ src/main/kotlin/com/example/spring_boot_prec/presentation/interceptor/PresentationInterceptorConfig.kt
@@ -1,6 +1,26 @@
 package com.example.spring_boot_prec.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfig
+class PresentationInterceptorConfig(
+  private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+
+  override fun addInterceptors(registry: InterceptorRegistry) {
+    registry.addInterceptor(PresentationInterceptor)
+      .addPathPatterns("/**")
+      .excludePathPatterns(
+        "/assets/**",
+        "/css/**",
+        "/js/**",
+        "/images/**",
+        "/admin/**",
+        "h2**",
+        "/favicon.ico",
+        "/error"
+      )
+  }
+}
