e0cf92714414e806d3efbf2e3c056e92a2650e29
HyungwonJin <hwjin92@gmail.com>
2025-01-04 23:08:38 +0900

    feat: creat DTO

--- src/main/kotlin/com/example/spring_boot_prec/admin/data/ApiResponse.kt
+++ src/main/kotlin/com/example/spring_boot_prec/admin/data/ApiResponse.kt
@@ -3,4 +3,19 @@ package com.example.spring_boot_prec.admin.data
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
-class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status)
+class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status) {
+
+  companion object {
+    fun successCreate(): ResponseEntity<Any> {
+      return ok("Created")
+    }
+
+    fun successUpdated(): ResponseEntity<Any> {
+      return ok("Updated")
+    }
+
+    fun successDeleted(): ResponseEntity<Any> {
+      return ok("Deleted")
+    }
+  }
+}
