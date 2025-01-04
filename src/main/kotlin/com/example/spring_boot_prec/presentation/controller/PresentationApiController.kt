4c2f85e4aefd2aca5cfc9592eb2d122b276d38f0
HyungwonJin <hwjin92@gmail.com>
2025-01-04 19:11:01 +0900

    feat: implements controller

--- src/main/kotlin/com/example/spring_boot_prec/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/example/spring_boot_prec/presentation/controller/PresentationApiController.kt
@@ -1,8 +1,37 @@
 package com.example.spring_boot_prec.presentation.controller
 
+import com.example.spring_boot_prec.presentation.dto.IntroductionDTO
+import com.example.spring_boot_prec.presentation.dto.LinkDTO
+import com.example.spring_boot_prec.presentation.dto.ProjectDTO
+import com.example.spring_boot_prec.presentation.dto.ResumeDTO
+import com.example.spring_boot_prec.presentation.service.PresentationService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController
+class PresentationApiController(
+  private val presentationService: PresentationService
+) {
+
+  @GetMapping("/v1/introductions")
+  fun getIntroductions(): List<IntroductionDTO> {
+    return presentationService.getIntroductions()
+  }
+
+  @GetMapping("/v1/links")
+  fun getLinks(): List<LinkDTO> {
+    return presentationService.getLinks()
+  }
+
+  @GetMapping("/v1/resume")
+  fun getResume(): ResumeDTO {
+    return presentationService.getResume()
+  }
+
+  @GetMapping("/v1/projects")
+  fun getProject(): List<ProjectDTO> {
+    return presentationService.getProjects()
+  }
+}
